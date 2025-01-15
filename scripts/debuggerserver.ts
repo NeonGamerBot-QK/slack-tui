5a53ad2f67d1c96108d40fd58f6646bd17b6ae9f
Saahil <neon@saahild.com>
2025-01-14 21:06:01 -0500

    before i break everything

---
+++ scripts/debuggerserver.ts
@@ -0,0 +1,8 @@
+import http from "http"
+const server = http.createServer((req,res) => {
+    // pipe body to console
+    req.pipe(process.stdout)
+    res.end()
+})
+
+server.listen(3000)
--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,13 @@ const app = new App({
     socketMode: true,
     appToken: process.env.APP_TOKEN,
 })
+const debugFunc = (text:string) => {
+  if(!process.env.ALLOW_NEONS_DANGEROUS_DEBUG) return;
+  fetch('http://localhost:3000', {
+    method: 'POST',
+    body: text + "\n"
+  })
+}
 export async function getUsername(user_id:string){
   const cachedName = getCachedUsers().find((e:any)=>e.id == user_id)?.name
   if(cachedName) return cachedName
@@ -115,10 +122,19 @@ async function screenMain() {
             border: {
               fg: '#f0f0f0'
             },
+            selected: {
+              fg: "white",
+              bg: "grey"
+            }
           //   hover: {
           //     bg: 'green'
           //   }
-          }
+          
+          },
+          scrollable: true,        // Enable scrolling
+          alwaysScroll: true,      // Always show scroll
+          keys: true,              // Enable keyboard control
+          mouse: true,   
         });
         // console.log(fake_channels[0])
          const channelBox = blessed.list({
@@ -149,13 +165,15 @@ async function screenMain() {
           //   }
           },
           items: fake_channels.map((e:any)=>{
+            if(Array.isArray(e)) return false;
             if(e.name) {
                 return `#${e.name}`
             } else {
-                return `@${cached_users.find((d:any)=>d.id == e.user)?.name}`
+              debugFunc(JSON.stringify(e, null, 2))
+                return `@${cached_users.find((d:any)=>d.id == e.user)?.name || "i d o n t k n o w"}`
             }
 
-          })
+          }).filter(Boolean) as string[]
         });
         // chat input to send messages
         const chatInput =blessed.textbox({
@@ -189,12 +207,16 @@ async function screenMain() {
             //   }
             }
         })
+        let global_messages:any[] = []
         async function renderMessages(channel_id: string){
             // console.debug(`#${channel_id}`)
             // clear the chats
             chats.setContent("")
             chats.clearItems()
-          for(const message of ((await app.client.conversations.history({ channel: channel_id, limit: 100 }).then(e=>e.messages!)).reverse())){
+            
+            const msgs = (await app.client.conversations.history({ channel: channel_id, limit: 100 }).then(e=>e.messages!)).reverse()
+            global_messages = msgs
+            for(const message of msgs){
             // console.debug(0)
               // const messageBox = blessed.box({
               //   parent: chats,
@@ -234,6 +256,7 @@ async function screenMain() {
               }
               // if(message )
           }
+          chats.select(-1)
           screen.render();
         }
         chatInput.key(['enter'], function(ch, key) {
@@ -264,11 +287,13 @@ async function screenMain() {
                  text: message || "broken message"
               })
            })
+           chats.add(`Me: ${message || "broken message"}`)
           //  console.log(1)
           }
           chatInput.clearValue();
           screen.render();
         });
+
         channelBox.on('select', function(ch, key) {
             // chats.pushLine(channel.name);
                      //@ts-ignore
@@ -305,9 +330,83 @@ async function screenMain() {
                   selected_channel = fake_channels[channelBox.selected].id
           // Update the selected channel when navigating
           // updateSelectedChannel(listbox.getItem(index).getContent());
+        } else if(focused == FocusedOn.Chat) {
+          if(key.name == "up") {
+            //@ts-ignore
+            if(chats.items.length > 0) {
+            //@ts-ignore
+            // console.log(chats.selectedItem)
+            //@ts-ignore
+              chats.select(chats.selected - 1)
+            }
+          } else if(key.name == "down") {
+            //@ts-ignore
+            if(chats.items.length > 0) {
+            //@ts-ignore
+              chats.select(chats.selected + 1)
+            }
+          }
         }
             screen.render();
           });
+          screen.key(['d'], function () {
+            if(focused == FocusedOn.Chat) {
+              // run a popup
+              const popup = blessed.box({
+                parent: screen,
+                top: "center",
+                left: "center",
+                width: "50%",
+                height: "50%",
+                content: "Are you sure you want to delete this message?",
+                tags: true,
+                border: {
+                  type: 'line'
+                },
+                style: {
+                  fg: 'white',
+                //   bg: 'magenta',
+                  border: {
+                    fg: '#f0f0f0'
+                  },
+                  hover: {
+                    bg: 'green'
+                  }
+                }
+              })
+              popup.key(['y'], function(ch, key) {
+                // chats.pushLine(chatInput.getValue().trim());
+                //TODO: you know like um send a real message
+                const message = chatInput.getValue().trim()
+                if(message.length <= 0) return;
+                // console.log(message)
+                if(selected_channel) {
+                 app.client.chat.delete({
+                  channel: selected_channel!,
+                  //@ts-ignore
+                  ts: global_messages[chats.selected]!.ts
+                })
+                //@ts-ignore
+                chats.removeItem(chats.selected)
+                }
+                popup.destroy()
+                screen.render();
+              })
+              popup.key(['n'], function(ch, key) {
+                popup.destroy()
+                screen.render();
+              })
+
+              // //@ts-ignore
+              // const ts = global_messages[chats.selected]!.ts
+              // app.client.chat.delete({
+              //   channel: selected_channel!,
+              //   ts: ts
+              // })
+              // //@ts-ignore
+              // chats.removeItem(chats.selected)
+            }
+          })
           screen.key(['tab'], function (ch, key) {
             focused = focused == FocusedOn.Channels ? FocusedOn.Chat : FocusedOn.Channels
               // channelBox.fg
@@ -318,51 +417,13 @@ async function screenMain() {
               // channelBox.render()
             screen.render();
           })
+
         // Append our box to the screen.
         screen.append(chats);
         screen.append(channelBox);
         screen.append(chatInput);
-        // for(const channel of fake_channels){
-        //   // channelBox(channel.name);
-        //   console.log(channel)
-        //   const channelBox0 = blessed.button({
-        //       parent: channelBox,
-        //       mouse: true,
-        //       keys: true,
-        //       // vi: true,
-        //       content: "#"+channel.name,
-        //       height: "10%",
-        //       padding: {
-        //         bottom: 1
-        //       },
-        //       // style: {
-        //       //   fg: 'green',
-        //       //   bg: 'green',
-        //       // //   bg: 'black',
-        //       //   border: {
-        //       //     fg: 'green'
-        //       //   },
-        //       // }
-        //       border: 'line',
-        //       style: {
-        //           fg: 'white',
-        //         //   bg: 'magenta',
-        //           border: {
-        //             fg: '#f0f0f0'
-        //           },
-        //             hover: {
-        //               bg: 'green'
-        //             }
-        //         }
-        //     })
-        //     channelBox0.on('press', function(data:any) {
-        //       // chats.pushLine(channel.name);
-        //   //   console.log(channel.name);
-        //     channelBox0.setContent("#"+`fucku`);
-        //     renderMessages(channel.id)
-        //       screen.render();
-        //     });
-        // }
+        //@ts-ignore
+          renderMessages(fake_channels[channelBox.selected].id)
   screen.render();
     }
 main()
